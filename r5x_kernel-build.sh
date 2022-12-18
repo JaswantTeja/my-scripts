19c1c8cf4d6ef6803d86061717ab8467c364f6f8
Jaswanth Teja <sunnyteja201@gmail.com>
2022-12-18 19:12:54 +0530

    change the defconfig

--- r5x_kernel-build.sh
+++ r5x_kernel-build.sh
@@ -49,7 +49,7 @@ DEVICE="Realme 5 Series (r5x)"
 export DEVICE
 CODENAME="r5x"
 export CODENAME
-DEFCONFIG="vendor/trinket_oppo/trinket-perf_defconfig"
+DEFCONFIG="vendor/RMX1911_defconfig"
 export DEFCONFIG
 COMMIT_HASH=$(git rev-parse --short HEAD)
 export COMMIT_HASH
