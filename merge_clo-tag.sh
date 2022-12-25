bbb7e7cb57dd2c0aec0f879e328ee8e7f948a09e
Jaswanth Teja <sunnyteja201@gmail.com>
2022-12-25 20:01:20 +0530

    clo-merge: add .git for urls

--- merge_clo-tag.sh
+++ merge_clo-tag.sh
@@ -1,25 +1,25 @@
 #!/usr/bin/env bash
 
 gsa() {
-	git remote add qcacld-3.0 https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0
+	git remote add qcacld-3.0 https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0.git
 	git fetch qcacld-3.0 $tag
 	git merge -s ours --no-commit --allow-unrelated-histories FETCH_HEAD
 	git read-tree --prefix=drivers/staging/qcacld-3.0 -u FETCH_HEAD
 	git commit -s -m "Merge tag '${tag}' of https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qcacld-3.0"
 
-	git remote add fw-api https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/fw-api
+	git remote add fw-api https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/fw-api.git
 	git fetch fw-api $tag
 	git merge -s ours --no-commit --allow-unrelated-histories FETCH_HEAD
 	git read-tree --prefix=drivers/staging/fw-api -u FETCH_HEAD
 	git commit -s -m "Merge tag '${tag}' of https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/fw-api"
 
-	git remote add qca-wifi-host-cmn https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn
+	git remote add qca-wifi-host-cmn https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn.git
 	git fetch qca-wifi-host-cmn $tag
 	git merge -s ours --no-commit --allow-unrelated-histories FETCH_HEAD
 	git read-tree --prefix=drivers/staging/qca-wifi-host-cmn -u FETCH_HEAD
 	git commit -s -m "Merge tag '${tag}' of https://git.codelinaro.org/clo/la/platform/vendor/qcom-opensource/wlan/qca-wifi-host-cmn"
 
-	git remote add audio-kernel https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel
+	git remote add audio-kernel https://git.codelinaro.org/clo/la/platform/vendor/opensource/audio-kernel.git
 	git fetch audio-kernel $tag
 	git merge -s ours --no-commit --allow-unrelated-histories FETCH_HEAD
 	git read-tree --prefix=techpack/audio -u FETCH_HEAD
